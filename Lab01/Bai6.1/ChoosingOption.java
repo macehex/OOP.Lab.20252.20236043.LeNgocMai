d047051494c0faed7f54cc587d2887519a5c99b2
unknown <macehelix@protonmail.com>
2025-03-05 21:42:42 +0700

    .

--- Lab01/Bai6.1/ChoosingOption.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-public class Bai61ChoosingOption {
-    public static void main(String[] args) {
-        int option = JOption.Pane.showConfirmDialog(null, "Do you want the first class ticket?");
-        javax.swing.JOptionPane.showMessageDialog(null, "You've chosen: "+
-                (option == JOptionPane.YES_OPTION ? "Yes" : "No"));
-        System.exit(0);
-    }
-}
