258745a8d2eadbfdd48f762a56cb4b8e175e193f
unknown <macehelix@protonmail.com>
2025-03-05 21:37:13 +0700

    updates

--- Lab01/JavaBasics/src/Bai64DaysOfAMonth.java
+++ Lab01/JavaBasics/src/Bai64DaysOfAMonth.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
 public class Bai64DaysOfAMonth {
+    Scanner keyboard = new Scanner(System.in);
+
+    int inputmonth = Integer.valueOf(keyboard.nextLine());
+    int inputyear = Integer.valueOf(keyboard.nextLine());
+
 
 }
