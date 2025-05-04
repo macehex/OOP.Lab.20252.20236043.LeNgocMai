716f5aedbfdba53cf447b1a8c5f8c3cb23d76074
unknown <macehelix@protonmail.com>
2025-05-04 20:55:45 +0700

    Add store

--- AimsProject/Requirement/src/hust/soict/itep/aims/store/Store.java
+++ AimsProject/Requirement/src/hust/soict/itep/aims/store/Store.java
@@ -1,4 +1,18 @@
 package hust.soict.itep.aims.store;
 
+import hust.soict.itep.aims.DigitalVideoDisc;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Store {
+    ArrayList<DigitalVideoDisc> itemsInStore = new ArrayList<>();
+    public void addDVD(DigitalVideoDisc dvd){
+        itemsInStore.add(dvd);
+        System.out.println("Added " + dvd.getTitle() + " to the store");
+    }
+    public void removeDVD(DigitalVideoDisc dvd){
+        itemsInStore.remove(dvd);
+        System.out.println("Removed " + dvd.getTitle() + " from the store");
+    }
 }
---
+++ AimsProject/Requirement/src/hust/soict/itep/aims/store/StoreTest.java
@@ -0,0 +1,16 @@
+package hust.soict.itep.aims.store;
+
+import hust.soict.itep.aims.DigitalVideoDisc;
+
+public class StoreTest {
+    public static void main(String[] args) {
+//        He or she must provide all information about the new DVD,
+//        including its ID, title, category, director, length, and the cost.
+//        Additionally, the manager can also remove DVDs from the store.
+        Store store = new Store();
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers",4, 28.15f);
+        store.addDVD(dvd1);
+        store.removeDVD(dvd1);
+    }
+
+}
